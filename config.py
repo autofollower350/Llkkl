e098091e93a46480279866a7c8e9710627c810f7
autofollower350 <followerauto350@gmail.com>
2025-02-23 08:12:49 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,5 @@
-# config.py
+import os  
 
-import os
-
-API_ID = int(os.getenv("API_ID", "28590286"))  # अपने Telegram API ID से बदलें
-API_HASH = os.getenv("API_HASH", "6a68cc6b41219dc57b7a52914032f92f)  # अपने Telegram API Hash से बदलें
-BOT_TOKEN = os.getenv("BOT_TOKEN", "7412939071:AAFk6_MJziu22UnZOv9crH-HcwLDqzQ1SVg)  # अपने Telegram Bot Token से बदलें
+API_ID = os.getenv("API_ID")  
+API_HASH = os.getenv("API_HASH")  
+BOT_TOKEN = os.getenv("BOT_TOKEN")
