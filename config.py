a9c4e990b6561b6138c27745c20846441b8ab5ef
autofollower350 <followerauto350@gmail.com>
2025-02-23 08:23:44 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,6 @@
-import os  
+import os
 
-API_ID = os.getenv("API_ID")  
-API_HASH = os.getenv("API_HASH")  
-BOT_TOKEN = os.getenv("BOT_TOKEN")
+API_ID = 28590286  # अपनी API ID यहाँ डालें
+API_HASH = "6a68cc6b41219dc57b7a52914032f92f"  # अपनी API Hash यहाँ डालें 
+BOT_TOKEN = "7949674880:AAF6FtDotEnZ5m1NEK0cE7YKNHK_tisd8Mk"  # अपना Bot Token यहाँ डालें
+
